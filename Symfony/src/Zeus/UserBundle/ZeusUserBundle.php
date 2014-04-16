36ccf4e7759846e02a8038393da743d834e4805b
GUICHERD Kévin <kevinguicherd@gmail.com>
2014-04-16 19:57:33 +0200

    Modification du bundlle User

--- Symfony/src/Zeus/UserBundle/ZeusUserBundle.php
+++ Symfony/src/Zeus/UserBundle/ZeusUserBundle.php
@@ -6,8 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ZeusUserBundle extends Bundle
 {
-	public function getParent()
-	{
-		return 'FOSUserBundle';
-	}
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
